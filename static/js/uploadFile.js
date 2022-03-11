80bf1d44368593137dd6bcd36a1d425e951ffce9
Mirumo <mysteryperson520@gmail.com>
2022-03-11 10:59:42 +0800

    upload file user interface

---
+++ static/js/uploadFile.js
@@ -0,0 +1,26 @@
+window.onload = function () {
+  const label = document.querySelector('#file-upload-label');
+  label.addEventListener('dragover', (e) => e.preventDefault());
+  label.addEventListener('dragleave', (e) => e.preventDefault());
+  label.addEventListener('drop', (e) => {
+    e.preventDefault();
+    const { files } = e.dataTransfer;
+    uploadFiles(files);
+  });
+};
+
+function handleFormChange(e) {
+  const { files } = e.target;
+  uploadFiles(files);
+  e.target.value = null;
+}
+
+function uploadFiles(files) {
+  let formData = new FormData();
+  for (let file of files) formData.append('uploadFile', file);
+
+  fetch('/upload', {
+    method: 'POST',
+    body: formData
+  }).then((response) => console.log(response));
+}
